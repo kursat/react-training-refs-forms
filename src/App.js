54e0380c22c8651fcfcb1015c032fcc6aeb15ad5
Kürşat Yiğitoğlu <kursat.yigitoglu@gmail.com>
2024-02-24 15:37:09 +0300

    formik and yup

--- src/App.js
+++ src/App.js
@@ -1,46 +1,92 @@
 import { Button, Input } from '@nextui-org/react';
 import { useRef, useState } from 'react';
+import { useFormik } from 'formik';
+import * as yup from 'yup';
+
+// Form libraries
+// https://react-hook-form.com/
+// https://formik.org/
+
+const SignupSchema = yup.object().shape({
+    firstName: yup
+        .string()
+        .min(2, 'Too Short!')
+        .max(10, 'Too Long!')
+        .required('Required'),
+    lastName: yup
+        .string()
+        .min(2, 'Too Short!')
+        .max(10, 'Too Long!')
+        .required('Required'),
+
+    // mobileNumber: yup.string(),
+    //
+    // email: yup.string().when('mobileNumber', {
+    //     is: (mobileNumber) => mobileNumber !== '',
+    //     then: yup.string().nullable().required(),
+    // }),
+});
 
 function App() {
-    const [renderDiv, setRenderDiv] = useState(true);
+    const {
+        handleSubmit,
+        values,
+        dirty,
+        touched,
+        errors,
+        handleChange,
+        handleBlur,
+    } = useFormik({
+        initialValues: {
+            firstName: 'Kürşat',
+            lastName: 'Yiğitoğlu',
+        },
+        validationSchema: SignupSchema,
+        onSubmit: (values) => {
+            console.log(JSON.stringify(values, null, 2));
+        },
+    });
 
-    const [renderCount, setRenderCount] = useState(0);
-    const clickCount = useRef(0);
-    const inputRef = useRef();
+    // const handleBlur = (...rest) => {
+    //     console.log('test');
+    //
+    //     _handleBlur(...rest);
+    // };
 
-    const onClickButton = () => {
-        setRenderDiv(!renderDiv);
-
-        console.log('inputRef', inputRef);
-        inputRef.current.focus();
-        console.log('inputRef.current.value', inputRef.current.value);
-
-        setRenderCount(renderCount + 1);
-    };
-
-    const onClickDiv = () => {
-        clickCount.current++;
-    };
-
-    console.log('renderCount', renderCount);
-    console.log('clickCount', clickCount);
+    // console.log('values', values);
+    // console.log('touched', touched);
+    // console.log('dirty', dirty);
+    console.log('errors', errors);
 
     return (
         <div className="App">
-            {renderDiv && (
-                <div onClick={onClickDiv} className="w-40 h-40 bg-blue-200">
-                    Click here!
-                </div>
-            )}
-            <Button
-                onClick={onClickButton}
-                variant={'shadow'}
-                color={'secondary'}
+            <form
+                className="flex flex-col gap-2 w-72 m-2"
+                onSubmit={handleSubmit}
             >
-                Render
-            </Button>
-            <Input baseRef={inputRef} />
-            {/*<input className={'border-1 h-12'} ref={inputRef} />*/}
+                <Input
+                    label={'Adı'}
+                    name={'firstName'}
+                    value={values.firstName}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    isInvalid={!!errors.firstName}
+                    errorMessage={errors.firstName}
+                />
+                <Input
+                    label={'Soyadı'}
+                    name={'lastName'}
+                    value={values.lastName}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    isInvalid={!!errors.lastName}
+                    errorMessage={errors.lastName}
+                />
+
+                <Button color={'success'} type={'submit'}>
+                    Submit
+                </Button>
+            </form>
         </div>
     );
 }

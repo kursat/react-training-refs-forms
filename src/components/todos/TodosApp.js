b8a473fe9e55f76bf3d872e5d6c62942ec308060
Kürşat Yiğitoğlu <kursat.yigitoglu@gmail.com>
2024-02-26 12:06:20 +0300

    custom context provider

--- src/components/todos/TodosApp.js
+++ src/components/todos/TodosApp.js
@@ -1,31 +1,12 @@
-import React, { useReducer, useState } from 'react';
+import React from 'react';
 import TodoList from './TodoList';
-import { TodoContext, TodoDispatchContext } from '../../contexts/TodoContext';
-
-const reducer = (state, action) => {
-    switch (action.type) {
-        case 'add':
-            return [...state, action.payload];
-        case 'delete':
-            return state.filter((i) => i.id !== action.payload);
-        default:
-            return state;
-    }
-};
+import TodoProvider from '../../contexts/TodoProvider';
 
 const TodosApp = () => {
-    const [todos, dispatch] = useReducer(reducer, [
-        { id: 1, text: 'Todo 1' },
-        { id: 2, text: 'Todo 2' },
-        { id: 3, text: 'Todo 3' },
-    ]);
-
     return (
-        <TodoContext.Provider value={todos}>
-            <TodoDispatchContext.Provider value={dispatch}>
-                <TodoList />
-            </TodoDispatchContext.Provider>
-        </TodoContext.Provider>
+        <TodoProvider>
+            <TodoList />
+        </TodoProvider>
     );
 };
 
---
+++ src/contexts/TodoProvider.js
@@ -0,0 +1,33 @@
+import React, { useReducer } from 'react';
+import { TodoContext } from './TodoContext';
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'add':
+            return [...state, action.payload];
+        case 'delete':
+            return state.filter((i) => i.id !== action.payload);
+        default:
+            return state;
+    }
+};
+const TodoProvider = ({ children }) => {
+    const [todos, dispatch] = useReducer(reducer, [
+        { id: 1, text: 'Todo 1' },
+        { id: 2, text: 'Todo 2' },
+        { id: 3, text: 'Todo 3' },
+    ]);
+
+    return (
+        <TodoContext.Provider
+            value={{
+                todos,
+                dispatch,
+            }}
+        >
+            {children}
+        </TodoContext.Provider>
+    );
+};
+
+export default TodoProvider;
